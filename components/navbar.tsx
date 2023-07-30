e6e4355288442718bb0b9b998a5bb5c3bd6cda2b
i1ko <oleksiuk13@gmail.com>
2023-07-30 09:01:10 +0300

    feat(store-switcher): created and pre-configured

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,25 @@
-import {UserButton} from "@clerk/nextjs";
+import {auth, UserButton} from "@clerk/nextjs";
 import {MainNav} from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
+import {redirect} from "next/navigation";
+import prismadb from "@/lib/prismadb";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const {userId} = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId
+    }
+  })
+
   return (
     <div className={`border-b`}>
       <div className={`flex h-16 items-center px-4`}>
-        <div>
-          This will be a store switcher
-        </div>
+        <StoreSwitcher items={stores} />
         <div>
           This will be the routes
         </div>
