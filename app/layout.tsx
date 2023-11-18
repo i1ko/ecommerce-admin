96b1880d9b2634389b4c50b4928e56516d7635de
i1ko <oleksiuk13@gmail.com>
2023-11-18 20:13:30 +0200

    refactor(import): ordering

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import React from "react";
 import {ClerkProvider} from "@clerk/nextjs";
+import { Inter } from 'next/font/google'
 import {ModalProvider} from "@/providers/modal-provider";
 import {ToasterProvider} from "@/providers/toast-provider";
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,16 +14,18 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
-  return <ClerkProvider>
-    <html lang="en">
-      <body className={inter.className}>
-        <ToasterProvider />
-        <ModalProvider />
-        {children}
-      </body>
-    </html>
-  </ClerkProvider>
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
 }
