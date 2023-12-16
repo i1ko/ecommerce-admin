431928ddee539f12ff6adbd5d1345d60d4ce640d
i1ko <oleksiuk13@gmail.com>
2023-12-16 17:44:45 +0200

    fix(auth): problem with no displaying no public routes handled by adding afterAuth callback

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,16 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
-export default authMiddleware();
 
 export const config = {
     matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+export default authMiddleware({
+    publicRoutes: ["/"],
+    afterAuth: (auth) => {
+        console.log(auth);
+        return auth.isPublicRoute ? NextResponse.next() : undefined;
+    }
+})
+
