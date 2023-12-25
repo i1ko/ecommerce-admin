95583e8a68d8f646d944caf5dc61304caab0c6c6
i1ko <oleksiuk13@gmail.com>
2023-12-25 12:32:35 +0200

    feat(overview): page in process. Added few widgets

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,12 @@
-import React from "react";
 import prismadb from "@/lib/prismadb";
+import {CreditCard, DollarSign, Package} from "lucide-react";
+import {formatter} from "@/lib/utils";
+import {Heading} from "@/components/ui/heading";
+import {Separator} from "@/components/ui/separator";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import {getTotalRevenue} from "@/actions/get-total-revenue";
+import {getSalesCount} from "@/actions/get-sales-count";
+import {getStockCount} from "@/actions/get-stock-count";
 
 interface DashboardPageProps {
   params: {storeId: string}
@@ -8,16 +15,58 @@ interface DashboardPageProps {
 const DashboardPage: React.FC<DashboardPageProps> = async ({
   params
 }) => {
-  const store = await prismadb.store.findFirst({
-    where: {
-      id: params.storeId
-    }
-  })
+  const totalRevenue = await getTotalRevenue(params.storeId);
+  const salesCount = await getSalesCount(params.storeId);
+  const stockCount = await getStockCount(params.storeId);
+
   return (
-    <div>
-      Active Store: {store?.name}
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        {/*todo: localization */}
+        <Heading title="Dashboard" description="Overview of ypur store" />
+        <Separator />
+        <div className="grid grid-cols-3 gap-4">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {formatter.format(totalRevenue)}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCard className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {salesCount}
+              </div>
+            </CardContent>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Products in Stock
+              </CardTitle>
+              <Package className="h-4 w-4 text-muted-foreground" />
+            </CardHeader>
+            <CardContent>
+              <div className="text-2xl font-bold">
+                {stockCount}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default DashboardPage;
